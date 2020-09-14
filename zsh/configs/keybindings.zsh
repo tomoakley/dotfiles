bf865a7570cde942b0aab4af16d78a45ccebc68e
tomoakley <tomoakley@MiMAC002.local>
2020-09-14 22:11:10 +0100

    add some zsh keybindings

--- zsh/configs/keybindings.zsh
+++ zsh/configs/keybindings.zsh
@@ -1,17 +1,23 @@
-bindkey -v
+stty -ixon
+
 export KEYTIMEOUT=1 # reduce key delay to 0.1s
 
 # Use vim cli mode
+bindkey -v
+bindkey "^F" vi-cmd-mode
 bindkey '^P' up-history
 bindkey '^N' down-history
 
 # backspace and ^h working even after
 # returning from command mode
 bindkey '^?' backward-delete-char
-bindkey '^h' backward-delete-char
+bindkey '^H' backward-delete-char
 
 # ctrl-w removed word backwards
 bindkey '^w' backward-kill-word
 
 # ctrl-r starts searching history backward
 bindkey '^r' history-incremental-search-backward
+
+bindkey "^A" beginning-of-line
+bindkey "^E" end-of-line
