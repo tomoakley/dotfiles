6b9e5bc2b5001d9e021ce57cb0d721b791809db3
tomoakley <tom@tomoakley.me>
2021-12-10 23:10:53 +0000

    add dotbot config, more brew packages, etc

--- Brewfile
+++ Brewfile
@@ -1,3 +1,8 @@
+tap "homebrew/bundle"
+tap "homebrew/cask"
+tap "homebrew/core"
+tap "homebrew/services"
+
 cask qutebrowser
 cask alacritty
 cask react-native-debbugger
@@ -5,8 +10,14 @@ cask 1password
 cask 1password-cli
 cask android-studio
 
+brew docker
+brew docker-compose
+brew nvm
+brew yarn
+
 brew weechat
 brew neovim
 brew tmux
 brew hammerspoon
+brew dotbot
 
--- install.sh
+++ install.sh
@@ -6,19 +6,16 @@ sudo chown -R $(whoami):admin /usr/local
 brew doctor
 brew update
 
+## Install packages from brewfile
 brew bundle
 
-## Remove default dotfiles
-sudo rm -rf ~/.vim > /dev/null 2>&1
-sudo rm -rf ~/.vimrc > /dev/null 2>&1
-sudo rm -rf ~/.bashrc > /dev/null 2>&1
-sudo rm -rf ~/.tmux > /dev/null 2>&1
-sudo rm -rf ~/.tmux.conf > /dev/null 2>&1
-sudo rm -rf ~/.zsh_prompt > /dev/null 2>&1
-sudo rm -rf ~/.zshrc > /dev/null 2>&1
-sudo rm -rf ~/.gitconfig > /dev/null 2>&1
-sudo rm -rf ~/.alacritty.yml > /dev/null 2>&1
-sudo rm -rf ~/.weechat > /dev/null 2>&1
+## setup nvm and node versions
+mkdir ~/.nvm/
+chmod +x /usr/local/opt/nvm/nvm.sh
 
+nvm install 12
+nvm use 12
+nvm alias default 10
 
-
+## setup dotfiles correctly
+dotbot -c install.conf.yaml
