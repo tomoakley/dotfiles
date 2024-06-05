488eea8145e756a68614b60567239ef837020764
Tom Oakley <toakley@totallymoney.com>
2024-06-05 12:04:37 +0100

    only run script if the repo has been cloned locally

--- qutebrowser/gh-nvim.sh
+++ qutebrowser/gh-nvim.sh
@@ -3,20 +3,17 @@
 QT_URL=$1 # PyQt6.QtCore.QUrl('https://github.com/owner/repo/pulls')
 URL=$(echo $QT_URL | sed -n 's/^.*'\''\(.*\)'\''.*$/\1/p') # extract url out of above string
 
-# ${MYVAR#pattern}     # delete shortest match of pattern from the beginning
-# ${MYVAR##pattern}    # delete longest match of pattern from the beginning
-# ${MYVAR%pattern}     # delete shortest match of pattern from the end
-# ${MYVAR%%pattern}    # delete longest match of pattern from the end
+REPO=$(echo "$URL" | awk -F/ '{print $5}')
+PATH_TO_REPO=~/code/$REPO
 
-# TMUX_SESSION=$(basename "$REPO" | tr . _)
-# echo $TMUX_SESSION
+if [ ! -e $PATH_TO_REPO ]; then
+  exit
+fi
+
 open -a alacritty
 
-
-REPO=$(echo "$URL" | awk -F/ '{print $5}')
 TMUX_SESSION=$(echo "$REPO" | sed 's/\./\_/g')
 PR_NUMBER=$(echo "$URL" | awk -F/ '{print $7}')
-
 
 if [[ -z $PR_NUMBER ]]; then
   WINDOW_NAME="PRs"
@@ -24,7 +21,7 @@ if [[ -z $PR_NUMBER ]]; then
   if [[ -n $WINDOW_ID ]]; then
     /opt/homebrew/bin/tmux kill-window -t "$WINDOW_ID"
   fi
-  /opt/homebrew/bin/tmux new-window -n "$WINDOW_NAME" -t "$TMUX_SESSION" -c ~/code/$REPO
+  /opt/homebrew/bin/tmux new-window -n "$WINDOW_NAME" -t "$TMUX_SESSION" -c $PATH_TO_REPO
   /opt/homebrew/bin/tmux switch-client -t "=$TMUX_SESSION:=$WINDOW_NAME"
   /opt/homebrew/bin/tmux send-keys -t "=$TMUX_SESSION:=$WINDOW_NAME" '/opt/homebrew/bin/nvim -c ":silent Octo pr list"' Enter
 else
@@ -33,7 +30,7 @@ else
   if [[ -n $WINDOW_ID ]]; then
     /opt/homebrew/bin/tmux switch-client -t "=$TMUX_SESSION:=$WINDOW_NAME"
   else
-    /opt/homebrew/bin/tmux new-window -n "$WINDOW_NAME" -t "$TMUX_SESSION" -c ~/code/$REPO
+    /opt/homebrew/bin/tmux new-window -n "$WINDOW_NAME" -t "$TMUX_SESSION" -c $PATH_TO_REPO
     /opt/homebrew/bin/tmux switch-client -t "=$TMUX_SESSION:=$WINDOW_NAME"
     /opt/homebrew/bin/tmux send-keys -t "=$TMUX_SESSION:=$WINDOW_NAME" "/opt/homebrew/bin/nvim -c ':silent Octo pr edit $PR_NUMBER'" Enter
   fi
