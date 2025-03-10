91eefae6dffaa5f3a98be7456d702b05feb8cdc2
Tom Oakley <toakley@totallymoney.com>
2025-03-10 21:20:08 +0000

    add kagi hammerspoon binding to put the webview at the side of the screen

--- hammerspoon/menubar/kagi.lua
+++ hammerspoon/menubar/kagi.lua
@@ -1,10 +1,9 @@
 local menuBarItem = hs.menubar.new()
 
--- Create a WebView
 local webView = hs.webview.new({x = 0, y = 0, w = 800, h = 600})
     :windowStyle("resizable")
     :allowGestures(true)
-    :allowTextEntry(true)  -- Allow the WebView to behave like a standard window
+    :allowTextEntry(true)
     :level(hs.drawing.windowLevels.modalPanel)
     :closeOnEscape(true)
     :url("https://www.kagi.com/assistant")
@@ -15,7 +14,6 @@ local webViewHotkeys = {}
 
 local function enableWebViewHotkeys()
     if #webViewHotkeys == 0 then
-        -- Define hotkeys specific to the WebView
         table.insert(webViewHotkeys, hs.hotkey.bind({"cmd"}, "N", function()
             local jsCode = [[
               var button = document.querySelector('[title="New Thread"]');
@@ -23,7 +21,7 @@ local function enableWebViewHotkeys()
                   button.click();
               }
             ]]
-            webView:evaluateJavaScript(jsCode)  -- Copy selected text
+            webView:evaluateJavaScript(jsCode)
         end))
 
         table.insert(webViewHotkeys, hs.hotkey.bind({"cmd"}, "H", function()
@@ -33,18 +31,24 @@ local function enableWebViewHotkeys()
                     button.click();
                 }
             ]]
-            webView:evaluateJavaScript(jsCode)  -- Copy selected text
+            webView:evaluateJavaScript(jsCode)
         end))
 
         table.insert(webViewHotkeys, hs.hotkey.bind({"cmd"}, "I", function()
           local frame = webView:frame()
-          local point = hs.geometry.point((frame.x + frame.w)/2, (frame.y + frame.h)/2) -- Replace with the coordinates of your textarea
+          local point = hs.geometry.point((frame.x + frame.w/2), (frame.y + frame.h - 30))
           hs.eventtap.leftClick(point)
         end))
 
-        table.insert(webViewHotkeys, hs.hotkey.bind({"cmd", "alt"}, "Q", function()
-            webView:hide()
-            disableWebViewHotkeys()  -- Disable hotkeys when hiding the WebView
+        table.insert(webViewHotkeys, hs.hotkey.bind({"cmd", "alt"}, "L", function()
+          local screenFrame = hs.screen.mainScreen():frame()
+          local webViewFrame = webView:frame()
+          webView:frame({ w = 400, h = screenFrame.h })
+
+          webViewFrame = webView:frame()
+          local x = screenFrame.w - webViewFrame.w
+          local newY = screenFrame.y + ((screenFrame.h - webViewFrame.h) / 2)
+          webView:topLeft({ x = x, y = newY })
         end))
     end
 end
@@ -52,12 +56,10 @@ end
 
 local function disableWebViewHotkeys()
     for _, hotkey in ipairs(webViewHotkeys) do
-        hotkey:delete()  -- Disable and delete the hotkey
+        hotkey:delete()
     end
-    webViewHotkeys = {}  -- Clear the hotkey list
+    webViewHotkeys = {}
 end
-
--- Function to toggle the WebView
 
 local function toggleWebView()
     if webView:isVisible() then
@@ -66,20 +68,19 @@ local function toggleWebView()
             outsideClickWatcher:stop()
             outsideClickWatcher = nil
         end
-        disableWebViewHotkeys()  -- Disable WebView-specific hotkeys
+        disableWebViewHotkeys()
     else
-        local screenFrame = hs.screen.mainScreen():frame()
         local menuBarFrame = menuBarItem:frame()
         local x = menuBarFrame.x - 400
         local y = menuBarFrame.h
+        webView:frame({ w = 800, h = 600 })
         webView:topLeft({x = x, y = y})
         webView:show()
         webView:bringToFront(true)
-        enableWebViewHotkeys()  -- Enable WebView-specific hotkeys
+        enableWebViewHotkeys()
     end
 end
 
--- Set up the menu bar item
-menuBarItem:setTitle("🌐")
+menuBarItem:setTitle("k")
 menuBarItem:setClickCallback(toggleWebView)
 hs.hotkey.bind({"cmd", "alt"}, "K", toggleWebView)
