35d993e25f80adfd768a7fc03ec2c4d7fb77ff39
Tom Oakley <toakley@totallymoney.com>
2025-06-27 11:23:57 +0100

    add tiny-code-actions to nvim plugins

---
+++ nvim/lua/tomoakley/plugins/tiny-code-actions.lua
@@ -0,0 +1,18 @@
+return {
+    "rachartier/tiny-code-action.nvim",
+    dependencies = {
+        {"nvim-lua/plenary.nvim"},
+    },
+    event = "LspAttach",
+    opts = {
+        picker = {
+            "buffer",
+            opts = {
+                hotkeys = true,
+                hotkeys_mode = "text_based",
+                position = "cursor"
+            }
+        },
+        backend = "vim",
+    },
+}
