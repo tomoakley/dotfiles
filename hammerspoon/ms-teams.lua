d79e92ee3448321fc250bd8dbcc3da3d9602e7fe
Tom Oakley <toakley@totallymoney.com>
2025-11-10 12:26:17 +0000

    add more MS Teams HS shortcuts

--- hammerspoon/ms-teams.lua
+++ hammerspoon/ms-teams.lua
@@ -9,7 +9,7 @@ local function getFocusedElementRole()
   if not elem then return false end
 
   local role = elem:role()
-  --print('DEBUG: focused element role: ' .. role)
+  -- print('DEBUG: focused element role: ' .. role)
   return role
 end
 
@@ -25,22 +25,53 @@ teamsKeyHandler = hs.eventtap.new({hs.eventtap.event.types.keyDown}, function(ev
   axApp:setAttributeValue('AXEnhancedUserInterface', true)
 
   local elementRole = getFocusedElementRole()
-  if elementRole == "AXTextField" or elementRole == "AXTextArea" or elementRole == "AXComboBox" or elementRole == "AXGroup" then
+
+  if elementRole == "AXTextField" or elementRole == "AXTextArea" or elementRole == "AXComboBox" --[[ or elementRole == "AXGroup" ]] then
     return false
   end
 
   local keyCode = event:getKeyCode()
   local flags = event:getFlags()
+
+  if flags:containExactly({"shift"}) then
+    if elementRole == 'AXMenuItem' then
+      return false
+    end
+    if keyCode == hs.keycodes.map.j then
+        hs.eventtap.keyStroke({"cmd"}, "l")
+        hs.eventtap.keyStroke({}, "Down")
+      return true
+    end
+    if keyCode == hs.keycodes.map.k then
+        hs.eventtap.keyStroke({"cmd"}, "l")
+        hs.eventtap.keyStroke({}, "Up")
+    end
+  end
+
   if flags:containExactly({}) then
+    if elementRole == 'AXMenuItem' then
+      if keyCode == hs.keycodes.map.escape then
+        hs.eventtap.keyStroke({}, "escape")
+        hs.eventtap.keyStroke({}, "return")
+      end
+      return false
+    end
     if keyCode == hs.keycodes.map.u then
         hs.eventtap.keyStroke({"alt", "cmd"}, "u")
     end
     if keyCode == hs.keycodes.map.g then
         hs.eventtap.keyStroke({"cmd"}, "g")
     end
-    if keyCode == hs.keycodes.map.escape and elementRole == 'AXMenuItem' then
-        hs.eventtap.keyStroke({}, "escape")
-        hs.eventtap.keyStroke({}, "return")
+    if keyCode == hs.keycodes.map.j then
+        hs.eventtap.keyStroke({"cmd"}, "p")
+        hs.eventtap.scrollWheel({0, -10}, {}, "line") -- scroll down
+    end
+    if keyCode == hs.keycodes.map.k then
+        hs.eventtap.keyStroke({"cmd"}, "p")
+        hs.eventtap.scrollWheel({0, 10}, {}, "line") -- scroll up
+    end
+    if keyCode == hs.keycodes.map.i then
+        hs.eventtap.keyStroke({"cmd"}, "R")
     end
   end
   return false -- Let other keys through
